e491aaf5a6a8b09d15ac4bb723fd552d3cfe1aac
Tim Yoon <tyoon@ualberta.ca>
2023-04-03 02:44:08 -0600

    formatted project page, created scroll buttons

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ function myFunction() {
     } else {
       x.className = "navbar";
     }
-  } 
+  }
+
+function downFunction() {
+  var element = document.getElementById("project");
+  element.scrollIntoView();
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
