98ad239bc253b84759f630c4a86f175d16b55a3f
Tim Yoon <tyoon@ualberta.ca>
2023-04-06 00:13:30 -0600

    layout, color, navbar

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ function myFunction() {
     } else {
       x.className = "navbar";
     }
-  }
+}
 
-function downFunction() {
-  var element = document.getElementById("project");
-  element.scrollIntoView();
+function toggleNav() {
+  var navbar = document.getElementById("myNavbar");
+  navbar.style.width = navbar.style.width === "250px" ? '0' : '250px';
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("myNavbar").style.top = "0";
+  } else {
+    document.getElementById("myNavbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 }
 
 function topFunction() {
